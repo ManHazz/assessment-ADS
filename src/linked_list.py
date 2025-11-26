704463e233ad941e706227ed3df3944ef1971bbc
raziq <raziqsufian2002@gmail.com>
2025-11-26 18:54:56 +0800

    update on algorithm

--- src/linked_list.py
+++ src/linked_list.py
@@ -8,16 +8,13 @@ class LinkedList:
         self.head = None
 
     def insert(self, item_data):
-        """Inserts item at the beginning (O(1))"""
+        
         new_node = Node(item_data)
         new_node.next = self.head
         self.head = new_node
 
     def linear_search(self, target_id):
-        """
-        Baseline Algorithm: Linear Search
-        Time Complexity: O(n)
-        """
+       
         current = self.head
         steps = 0
         while current:
